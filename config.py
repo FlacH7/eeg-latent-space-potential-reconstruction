85ee082800163eb427d0b0321ab92a361e0471fc
FlacH7 <loidel.barrera@gmail.com>
2026-07-05 00:30:46 +0800

    adding configs

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 # load .env
 load_dotenv()
 
-# Acceso a variables de entorno
+# Environment variables
 DB_TEST_RETEST_PATH = os.getenv("DB_TEST_RETEST_PATH")
 DATA_SIMULATIONS_CACHE_PATH = os.getenv("DATA_SIMULATIONS_CACHE_PATH")
+BASE_CACHE_PATH = os.getenv("BASE_CACHE_PATH")
+BASE_RESULTS_PATH = os.getenv("BASE_RESULTS_PATH")
