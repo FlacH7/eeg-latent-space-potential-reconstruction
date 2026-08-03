0fb4f2955e5d3212090cf97949bfbe8d8fcf891d
FlacH7 <loidel.barrera@gmail.com>
2026-08-04 07:20:04 +0800

    solving params path bug

--- src/batch_runs/run_batch_multi_method.py
+++ src/batch_runs/run_batch_multi_method.py
@@ -144,7 +144,7 @@ else:
 # ---------------------------------------------------------------------------
 # Ruta por defecto del JSON de parametros
 # ---------------------------------------------------------------------------
-DEFAULT_PARAMS_JSON = Path(BASE_PARAMS_FILE) / "multimethod_call_params.json"
+DEFAULT_PARAMS_JSON = Path("multimethod_call_params.json")
 
 # ---------------------------------------------------------------------------
 # Lectura del JSON de parametros
