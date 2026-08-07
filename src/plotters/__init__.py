ba565b3584de8d0e58dd217ae6ba8645656e68b2
FlacH7 <loidel.barrera@gmail.com>
2026-08-07 10:18:26 +0800

    adding imports

--- src/plotters/__init__.py
+++ src/plotters/__init__.py
@@ -101,6 +101,8 @@ from src.plotters.pipeline_overview import (
 # ---------------------------------------------------------------------------
 from src.plotters._markov_helpers import compute_transition_matrix
 
+from trajectory_plots import plot_latent_trajectory
+
 
 __all__ = [
     # Style
@@ -141,4 +143,5 @@ __all__ = [
     "plot_pipeline_flowchart",
     # Helpers
     "compute_transition_matrix",
+    "plot_latent_trajectory"
 ]
