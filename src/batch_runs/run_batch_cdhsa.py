c8cbbf80dbf3e89ff3533bde4c98190ec4f1fe16
FlacH7 <loidel.barrera@gmail.com>
2026-08-14 14:30:53 +0800

    rework method to full cdhsa

--- src/batch_runs/run_batch_cdhsa.py
+++ src/batch_runs/run_batch_cdhsa.py
@@ -292,7 +292,10 @@ class CDHSABatchRunner:
 
     def __init__(self, params: dict, *, pipeline_script: Path | None = None) -> None:
         self.params = params
-        self.pipeline_script = pipeline_script or ("./src/pipelines/run_cdhsa.py")
+        self.pipeline_script = params.get(
+            "pipeline_module",
+            "src.pipelines.""run_cdhsa.py"
+        )
         self.ss_cfg = params["super_subjects"]
         self.exec_cfg = params.get("execution", {})
 
